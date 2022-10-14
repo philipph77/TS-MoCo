8370d1656595b6724c8989f3ef64f6d4c3d19795
Philipp Hallgarten <philipp.hallgarten@web.de>
2022-10-14 11:31:14 +0200

    initial commit

---
+++ print_model.py
@@ -0,0 +1,24 @@
+from architectures.TSMC import TSMC
+from modules.encoding_module import plEncodingModule
+
+if __name__ == '__main__':
+    encoder = TSMC(
+        input_features=62,
+        embedding_dim=50,
+        n_head_token_enc=10,
+        n_head_context_enc=10,
+        depth_context_enc=4
+    )
+
+    encoder_module = plEncodingModule(
+        encoder,
+        128,
+        1e-4,
+        0.9,
+        1,
+        0.5,
+        "channel_wise",
+        1
+    )
+
+    print(encoder_module)
