a55ea927ecb5bc83657e7918bfe13d8b90e6c83f
Philipp Hallgarten <philipp.hallgarten@web.de>
2022-10-18 09:23:33 +0200

    Refactor: Transformer Implementation

--- architectures/TSMC.py
+++ architectures/TSMC.py
@@ -1,28 +1,33 @@
 from turtle import forward
 import torch
 import torch.nn as nn
-from architectures.transformer import TokenPosTransformerEncoder, CLSTransformerEncoder, TransformerEncoder
+from architectures.transformer import TransformerEncoder
 from .customLayers import TemporalSplit, OnetoManyGRU
-
 
 class TSMC(nn.Module):
     def __init__(self, input_features: int, embedding_dim: int, n_head_token_enc: int, n_head_context_enc: int, depth_context_enc: int, max_predict_len: int) -> None:
         super(TSMC, self).__init__()
         self.max_predict_len = max_predict_len
-        self.token_encoder = TokenPosTransformerEncoder(
-            input_features,
-            embedding_dim,
-            n_head_token_enc,
+        self.token_encoder = TransformerEncoder(
+            use_tokenizer=True,
+            use_cls_token=False,
+            use_pos_embedding=True,
+            input_features=input_features,
+            embedding_dim=embedding_dim,
+            n_head=n_head_token_enc,
             depth=1
         )
-        self.context_encoder = CLSTransformerEncoder(
-            embedding_dim,
-            embedding_dim,
-            n_head_context_enc,
-            depth_context_enc
+        self.context_encoder = TransformerEncoder(
+            use_tokenizer=False,
+            use_cls_token=True,
+            use_pos_embedding=False,
+            input_features=embedding_dim,
+            embedding_dim=embedding_dim,
+            n_head=n_head_context_enc,
+            depth=depth_context_enc
         )
         self.temporal_split = TemporalSplit()
-        self.prediction_head = OnetoManyGRU(embedding_dim, batch_first=True)
+        self.prediction_head = OnetoManyGRU(embedding_dim, embedding_dim, batch_first=True)
 
     def forward(self, x: torch.tensor, K: int):
         tokens, _ = self.token_encoder(x)
