b00b9a2786a17c94573923ba13301282f6b63300
Philipp Hallgarten <philipp.hallgarten@web.de>
2022-11-10 15:33:33 +0100

    Change: task from regression to classification

--- datasets/dreamer_dataset.py
+++ datasets/dreamer_dataset.py
@@ -16,9 +16,9 @@ class DREAMERDataset(Dataset):
         self.X_train = dataset['X_train'].float()
         self.X_val = dataset['X_val'].float()
         self.X_test = dataset['X_test'].float()
-        self.Y_train = dataset['Y_train'].float()
-        self.Y_val = dataset['Y_val'].float()
-        self.Y_test = dataset['Y_test'].float()
+        self.Y_train = dataset['Y_train'].long().squeeze()-1
+        self.Y_val = dataset['Y_val'].long().squeeze()-1
+        self.Y_test = dataset['Y_test'].long().squeeze()-1
         if preprocessing=="normalize":
             self._normalize()
         elif preprocessing=="standardize":
@@ -52,7 +52,7 @@ class DREAMERDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.dataset = DREAMERDataset(self.datapath, preprocessing)
         self.input_features = 14
-        self.n_classes = 1
+        self.n_classes = 5
     
     def setup(self, stage):
         self.trainset = TensorDataset(self.dataset.X_train, self.dataset.Y_train)
